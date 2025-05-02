16aa98d1e38d266e0483ed521244ec7d96738f37
Linh Hoàng <vulinht1997@gmail.com>
2025-05-02 22:28:28 +0700

    add section 10 lab 2

---
+++ section-10-networking-services/configure-nginx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Update apt cache.
+sudo apt-get update
+
+#We need to add repo before installing nginx-core. Otherwise we get an error
+sudo add-apt-repository main
+sudo add-apt-repository universe
+sudo add-apt-repository restricted
+sudo add-apt-repository multiverse  
+
+# Install Nginx.
+sudo apt-get install -y nginx
+
+# Set the home page.
+echo "<html><body><h2>Welcome to Azure! My name is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
---
+++ section-10-networking-services/lab2-nsg.sh
@@ -0,0 +1,15 @@
+#Yêu cầu: Thực hiện cài đặt nginx trên máy chủ linux, sau đó tạo 1 NSG mở cổng 80, sau đó đóng cổng 80 lại và kiểm tra
+#Chuẩn bị: Một VNet có subnet, một Azure VM chạy Linux trong subnet đó, đã kết nối thành công. (tận dụng lại từ bài lab trước).
+#Bước 1: Mở Azure cloudshell hoặc Azure CLI (đã hướng dẫn ở chương trước), đăng nhập và chạy lệnh sau:
+
+az vm extension set \
+  --resource-group "[sandbox resource group name]" \
+  --vm-name my-vm \
+  --name customScript \
+  --publisher Microsoft.Azure.Extensions \
+  --version 2.1 \
+  --settings '{"fileUris":["https://raw.githubusercontent.com/vlhoang/microsoft-azure-for-beginner/master/section10-networking-services/configure-nginx.sh"]}' \
+  --protected-settings '{"commandToExecute": "./configure-nginx.sh"}'    
+
+#Bước 2: Tạo 1 NSG gán cho subnet của VM (theo hướng dẫn của video lab), mở cổng 80 và truy cập vào VM theo public IP, xem kết quả
+#Bước 3: Đóng cổng 80 và thử truy cập lại
