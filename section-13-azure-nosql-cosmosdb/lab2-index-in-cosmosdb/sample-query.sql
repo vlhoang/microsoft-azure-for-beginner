3711a68000fa3dc738b2e5a13426020e7508a563
vlhoang <vulinht1997@gmail.com>
2025-05-23 15:59:09 +0700

    Add section 13

---
+++ section-13-azure-nosql-cosmosdb/lab2-index-in-cosmosdb/sample-query.sql
@@ -0,0 +1,8 @@
+
+SELECT * FROM c where c.price = 2000
+
+SELECT * FROM c Order by c.price desc
+
+SELECT * FROM c where c.price < 1300 and c.category = 'Laptop'
+
+SELECT * FROM c where c.category = 'Laptop' Order by c.price asc
