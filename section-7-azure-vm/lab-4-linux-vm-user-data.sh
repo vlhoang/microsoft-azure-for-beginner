b4e06a4df248a310f2f66490611f2410dd03137b
Linh Hoàng <vulinht1997@gmail.com>
2025-08-17 21:10:21 +0700

    finalize

--- section-7-azure-vm/lab-4-linux-vm-user-data.sh
+++
@@ -1,9 +0,0 @@
-#Trong quá trình tạo Linux VM, các bạn copy code bên dưới cho vào section user-data để VM khi khởi động sẽ cài đặt các package cần thiết.
-# *Copy toàn bộ từ line 4 trở xuống:
-
-#!/bin/bash
-sudo apt update -y
-sudo apt install apache2 -y
-sudo systemctl start apache2
-sudo systemctl enable apache2
-echo "Welcome to Udemy" | sudo tee /var/www/html/index.html
